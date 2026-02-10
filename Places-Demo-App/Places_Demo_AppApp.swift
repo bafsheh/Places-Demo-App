ae2ad0b57abca057b55d03f91146dce36a5fbc59
Babak Afsheh <babak.afsheh@impalastudios.com>
2026-02-10 11:01:28 +0100

    Initial Commit

---
+++ Places-Demo-App/Places_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Places_Demo_AppApp.swift
+//  Places-Demo-App
+//
+//  Created by Bobi Afsheh on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Places_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Places-Demo-AppTests/Places_Demo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Places_Demo_AppTests.swift
+//  Places-Demo-AppTests
+//
+//  Created by Bobi Afsheh on 2/10/26.
+//
+
+import Testing
+@testable import Places_Demo_App
+
+struct Places_Demo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
